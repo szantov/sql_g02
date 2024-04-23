0f9ba4daf77d516ebab43124b9667fee7596fdc3
szantov <152182775+szantov@users.noreply.github.com>
2024-04-23 20:46:00 +0200

    Add files via upload

---
+++ hazi2.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Ugyfel](
+	[LOGIN] [nvarchar](255) NOT NULL,
+	[EMAIL] [nvarchar](255)  MASKED WITH (function = 'email()') NOT NULL,
+	[NEV] [nvarchar](255) MASKED WITH (Function = 'partial(1,"XXXXX",2)') NOT NULL,
+	[SZULEV] [int] MASKED WITH (FUNCTION = 'random(1,5)') NULL,
+	[NEM] [nvarchar](1) NULL,
+	[CIM] [nvarchar](255) MASKED WITH (Function = 'default()') NULL,
+ CONSTRAINT [PK_Ugyfel] PRIMARY KEY CLUSTERED 
+(
+	[LOGIN] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
